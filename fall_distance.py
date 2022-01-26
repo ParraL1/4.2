4281e02fa491426ec7e888f519e57e67d06ed8e9
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-01-26 13:02:36 -0600

    doesnt print

--- fall_distance.py
+++ fall_distance.py
@@ -7,4 +7,3 @@
 time = float(input("Enter the amount of time the object is falling"))
 def fall_distance(falling_time):
     return 0.5 * 9.8 *falling_time *falling_time
-print("Distance object has fallen is", fall_distance(time))
